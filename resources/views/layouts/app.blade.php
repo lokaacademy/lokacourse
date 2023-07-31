8bdf393ee16a6aefd976aea18bd5f1f649dbbc76
Zainal Farid <zainal@offeo.com>
2023-07-31 18:08:24 +0800

    apple loka icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#000000" />
         <link rel="shortcut icon" href="assets/images/loka-icon.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="assets/images/loka-icon.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="assets/images/loka-icon.jpg"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css"/>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}"> 
