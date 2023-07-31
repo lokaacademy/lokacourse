153c3034d2142b1bd5928a25280b558ac56cd070
Zainal Farid <zainal@offeo.com>
2023-07-31 13:56:05 +0800

    center logo

--- resources/views/sections/logo.blade.php
+++ resources/views/sections/logo.blade.php
@@ -1,4 +1,4 @@
-<div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+<div class="container px-4 mx-auto flex flex-wrap items-center justify-center">
     <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
         <img class="rounded-full opacity-25" src="assets/images/loka.jpeg" alt="loka academy logo" />
     </div>
